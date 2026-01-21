0686fa3e4ac9496139dd40b4caffd95686211a64
Opilane TTHK <opilane@tthk.ee>
2026-01-21 08:45:18 +0200

    e

--- Tsüklite_töö/e/Test/Program.cs
+++ Tsüklite_töö/e/Test/Program.cs
@@ -16,14 +16,17 @@
 
             int aastaarv = 0;
 
-            do
-            {
+            
+            
                 Console.WriteLine("Mis aasta on praegu?");
                 aastaarv = int.Parse(Console.ReadLine());
 
 
-            } while (aastaarv <= 2025 && aastaarv! <= 0);
-            Console.WriteLine($"Aasta on {aastaarv} ");
+             while (aastaarv <= 2025 && aastaarv! <= 0);
+            {
+                Console.WriteLine($"Aasta on {aastaarv} ");
+            }
+            while (aastaarv <= )
         }
     }
 }
