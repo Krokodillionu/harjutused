be6793d344fc844a518ca840693a213f63a73904
Opilane TTHK <opilane@tthk.ee>
2026-01-21 10:01:08 +0200

    shigma

--- Meetodid/Method/Program.cs
+++ Meetodid/Method/Program.cs
@@ -4,7 +4,110 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> filmid = GetMovies();
+            Console.WriteLine("Mis on sinu lemmikfilm");
+            string lemmikFilm = ReadAnswer();
+            DoesMovieExist(filmid, lemmikFilm);
+            filmid = DoYouLikeThisMovie(filmid, "Tron 1983");
+            foreach (string item in filmid)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            
+            
+            //foreach (string item in filmid)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // kirjuta programm mis
+            // küsib kasutajalt tema lemmikfilme, küsime ise on meetodiga, meetodi tulemus tagastatakse muutujasse
+            // küsib kasutajalt peale seda tema lemmikfilmi, vastus hoitakse muutujas
+        }
+
+
+        public static List<string> DoYouLikeThisMovie(List<string> collection, string movieToAdd)
+        {
+            if (collection.Contains(movieToAdd) == false)
+            {
+                Console.WriteLine($"Kas sulle meeldib {movieToAdd}?");
+                string vastus = ReadAnswer();
+                if (vastus == "jah")
+                {
+                    collection.Add(movieToAdd);
+                    Console.WriteLine("Lisasin filmi sulle");
+
+                }
+                else
+                {
+                    Console.WriteLine("Aga miks? See on ju hea film?");
+                }
+            }
+            return collection;
+        }
+
+
+
+
+        public static void DoesMovieExist(List<string> collection, string findThis)
+        {
+            bool itExists = false;
+            foreach (var item in collection)
+            {
+                if (item == findThis)
+                {
+                    itExists = true;
+                }
+                
+            }
+            if (itExists == true)
+            {
+                Console.WriteLine("Näe mäletasid ikka!!!!!");
+            }
+            else 
+            {
+                Console.WriteLine("kus sinu lemmikfilm?");
+            }
+        }
+
+        public static string ReadAnswer()
+        {
+            string sisend = "";
+            while (sisend == "")
+            {
+                sisend = Console.ReadLine();
+            }
+            return sisend;
+
+        }
+
+        public static List<string> GetMovies()
+        {
+
+            Console.WriteLine("Mis on sinu lemmikfilmid, sisesta ükshaaval, kui sisestada rohkem ei taha \n sisesta \"ei ole\"");
+            string sisestus = "";
+            List<string> siinOnFilmid = new List<string>();
+
+            while (sisestus != "ei ole")
+            {
+                Console.WriteLine("Järgmine film:");
+                sisestus = Console.ReadLine();
+                if (sisestus != "ei ole")
+                {
+                    siinOnFilmid.Add(sisestus);
+                }
+            }
+            return siinOnFilmid;
         }
     }
 }
+
+
+
+
+
+
+
