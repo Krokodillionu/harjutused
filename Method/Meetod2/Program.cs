95a0c2ba33a17a517ea820ecf67abd54f2b6f96f
Opilane TTHK <opilane@tthk.ee>
2026-01-29 10:01:51 +0200

    e

---
+++ Method/Meetod2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Meetod2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vastus = "";
+            string info = "";
+            do
+            {
+                Console.WriteLine("Palun sisesta filtreeritav lähteinfo");
+                info = GetResponse();
+                Console.WriteLine("Palun sisesta otsitav info");
+                string searchThisWord = GetResponse();
+                // filtreerimine
+                bool doesWordExist = FindThisWord(searchThisWord, info);
+                if (doesWordExist == true)
+                {
+                    Console.WriteLine("Leidsime sõna \"" + searchThisWord + "\" sinu sisestatud infost:");
+                    Console.WriteLine(info);
+                }
+                else
+                {
+                    Console.WriteLine("Sõna \"" + searchThisWord + "\"infost puudub");
+                }
+                vastus = GetResponse();
+            } while (vastus == "jah");
+
+            
+            do
+            {
+                Console.WriteLine("Kas soovid infosse midagi juurde lisada?");
+                vastus = GetResponse();
+                if (vastus == "jah")
+                {
+                    Console.WriteLine("Kirjuta lisatav info: ");
+                    info += GetResponse();
+                }
+                vastus = "";
+                Console.WriteLine("Kas tahad tegevust korrata?");
+                vastus = GetResponse();
+            } while (vastus == "jah");
+
+            do
+            {
+                Console.WriteLine("Kas salvestad dokumendi töölauale või dokumendikausta?");
+                string kuhu = GetResponse();
+                string saveFileHere = "";
+                if (kuhu == "töölaud")
+                {
+                    saveFileHere = "\"C:\\Users\\opilane\\Desktop\"";
+                }
+                else if (kuhu == "dokumendid")
+                {
+                    saveFileHere = "C:\\Users\\opilane\\Documents";
+                }
+
+
+
+
+
+
+
+
+            } while (vastus == "jah");
+            
+            // programmi lõpp
+            //Console.WriteLine("Headaega");
+        }
+
+        private static string RepeatAction()
+        {
+            string vastus;
+            Console.WriteLine("Kas tahad tegevust korrata? (jah/ei)");
+            vastus = GetResponse();
+            return vastus;
+        }
+        
+
+        public static bool FindThisWord(string filter, string toBeFiltered)
+        {
+            if (toBeFiltered.Contains(filter))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static string GetResponse()
+        {
+            string sisestus = "";
+            while (sisestus == "")
+            {
+                sisestus = Console.ReadLine();
+            }
+            return sisestus;
+        }
+    }
+}
